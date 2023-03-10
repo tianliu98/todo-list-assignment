bfa99b84302ac1f4841ddd9be28f208dc856ec27
tianliu98 <lt6363185@gmail.com>
2023-03-10 16:28:47 -0600

    fix svg bug

--- index.js
+++ index.js
@@ -135,11 +135,9 @@ const View = (() => {
                 const todoTemplate = `  <li>
                                         <span>${todo.title}</span>
                                         <button class="btn--edit" id="${todo.id}">
-                                        <div class = "svg--edit">
                                         <svg class="svg--edit" focusable="false" aria-hidden="true" viewBox="0 0 24 24"  aria-label="fontSize small">
                                             <path class="svg--delete" d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
                                         </svg>
-                                        </div>
                                         
                                         </button>
                                         
